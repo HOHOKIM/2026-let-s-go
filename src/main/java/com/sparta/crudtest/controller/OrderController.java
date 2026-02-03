f17b3ae01890f4321d6c2a17d2c66cd40599aa88
HOHOKIM <hagenith@naver.com>
2026-02-03 14:59:36 +0900

    주문 쫌 어렵네
    Signed-off-by: HOHOKIM <hagenith@naver.com>

--- src/main/java/com/sparta/crudtest/controller/OrderController.java
+++ src/main/java/com/sparta/crudtest/controller/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class OrderController {
@@ -24,23 +25,36 @@ public class OrderController {
 
     @PostMapping("/orders/{id}")
     // 아이디 오류 생겨서 Long 에서 Integer로 바뀐거 생각하기
-    public String createOrder(@PathVariable Integer id, Model model) {
+    public String createOrder(@PathVariable Integer id, Model model, RedirectAttributes redirectAttributes) {
         // 상품 조회
         Product product = productRepository.findById(id).orElseThrow();
+
+        if (product.getStock() <= 0){
+            redirectAttributes.addFlashAttribute("errorMessage","재고없음");
+            return "redirect:/product-list";
+        }
+
         // 주문 객체 생성
         Order order = new Order();
         order.setProductId(Long.valueOf(product.getId()));
         order.setName(product.getName());
+        order.setDescription(product.getDescription());
         order.setPrice(product.getPrice());
         order.setStock(product.getStock());
-        order.setDescription(product.getDescription());
-
+        order.setStock(order.getStock() -1);
         orderRepository.save(order);
 
-        return "redirect:/product-list"; // 저장 후 메인으로 이동 (원하면 /orders로 바꿀 수 있음)
+        // 주문시 -1식 감소하게 만들기
+        //  오더 , 프로덕트 테이블에서 스톡 가져와서 주문 = -1
+
+        product.setStock(product.getStock() - 1);
+        productRepository.save(product);
+
+        return "redirect:/product-list"; // 저장 후 메인으로 이동
         }
         // 주문 페이지로 이동
-        //복습하기 컨트롤러에서 매핑이름 정하기 -> 그다음 템플릿에서 겟팅(매핑)해서 연결하기
+
+    //복습하기 컨트롤러에서 매핑이름 정하기 -> 그다음 템플릿에서 겟팅(매핑)해서 연결하기
         @GetMapping("/product-order")
         public String  orderList(Model model){
         model.addAttribute("orders",orderRepository.findAll());
